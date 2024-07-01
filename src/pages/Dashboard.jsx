c891108aaa1e534b860290baaf31c0a7f486abdb
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-01 18:54:11 +0100

    added an outlet

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import styles from "./Dashboard.module.css";
 import Sidebar from "../components/Sidebar";
+import { Outlet } from "react-router-dom";
 
 function Dashboard() {
   return (
     <header className={styles.header}>
       <Sidebar />
+      <Outlet />
     </header>
   );
 }
