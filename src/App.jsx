63995a7bf60a7c85a5d1d4093f581694ce03b2ba
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-02 10:45:15 +0100

    exported the NewAnalysis component and imported it to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import NewAnalysis from "./components/NewAnalysis";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />}>
-          <Route index element={<p>Analyze</p>} />
-          <Route path="new-analysis" element={<p>Analyze</p>} />
+          <Route index element={<NewAnalysis />} />
+          <Route path="new-analysis" element={<NewAnalysis />} />
           <Route path="patient-record" element={<p>records</p>} />
         </Route>
       </Routes>
---
+++ src/components/NewAnalysis.jsx
@@ -0,0 +1,5 @@
+function NewAnalysis() {
+  return <div>Hello World</div>;
+}
+
+export default NewAnalysis;
