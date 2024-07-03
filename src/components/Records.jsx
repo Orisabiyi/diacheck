e043960f9538d19eed02ac6790412c4d6df351ef
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-03 09:17:38 +0100

    added the row for data

--- src/components/Records.jsx
+++ src/components/Records.jsx
@@ -18,6 +18,17 @@ function Records() {
           <th>Blood Glocuse Level</th>
           <th>Diabetes Status</th>
         </tr>
+        <tr>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+        </tr>
       </table>
     </section>
   );
