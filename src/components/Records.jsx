744ca0a076bffe218e0969cf3b0c663c5db949ee
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-03 09:09:13 +0100

    created a table for the patient record and imported the css module for styling

--- src/components/Records.jsx
+++ src/components/Records.jsx
@@ -1,5 +1,26 @@
+import styles from "./Records.module.css";
+
 function Records() {
-  return <section>Hello World</section>;
+  return (
+    <section className={styles.sectionRecord}>
+      <h1>Patient Records</h1>
+
+      <table>
+        <tr>
+          <th>Patient ID</th>
+          <th>Patient Name</th>
+          <th>Patient Age</th>
+          <th>Gender</th>
+          <th>Hypertension</th>
+          <th>Smoking History</th>
+          <th>BMI</th>
+          <th>BMI</th>
+          <th>Blood Glocuse Level</th>
+          <th>Diabetes Status</th>
+        </tr>
+      </table>
+    </section>
+  );
 }
 
 export default Records;
