0b84e516149d3775909206e38dd4aebf584aaf8a
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-05 17:58:13 +0100

    include an image for patient results

---
+++ src/components/Result.jsx
@@ -0,0 +1,14 @@
+import styles from "./Result.module.css";
+
+function Result() {
+  return (
+    <div className={styles.result}>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <p>Positive</p>
+    </div>
+  );
+}
+
+export default Result;
